8f6536ed3fa7bf3a64d303fdb9860cacbaaef842
Pablo Massao Lins Bunno <regigigaspower2@gmail.com>
2023-09-11 10:29:15 -0300

    Delete projeto novo directory

--- projeto novo/js/main.js
+++
@@ -1,8 +0,0 @@
-import aluno from './aluno.js';
-import professor from './professor.js';
-
-let alunoObjeto = new aluno("Paulo", 16, 1310);
-let professorObjeto = new professor("Tonhão", 35, "engenharia de automoveis");
-
-console.log(alunoObjeto.apresentar());
-console.log(professorObjeto.apresentar());
